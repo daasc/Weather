480d5a30a2364c75cbaf9ce248abd2d9566f731e
daasc <paulojhol@gmail.com>
2022-02-28 09:12:07 -0300

    add all test in the store weather

--- store/weather.js
+++ store/weather.js
@@ -1,13 +1,34 @@
+import axios from 'axios'
 export const state = () => ({
   weatherObjs: null,
+  search: '',
 })
 
 export const mutations = {
   SET_WEATHER: (state, payload) => {
     state.weatherObjs = payload
   },
+  SET_SEARCH: (state, payload) => {
+    state.search = payload
+  },
 }
 
 export const getters = {}
 
-export const actions = {}
+export const actions = {
+  async getWeather({ commit, state }) {
+    // https://api.openweathermap.org/data/2.5/weather?q=Bras%C3%ADlia%20&appid=4d8fb5b93d4af21d66a2948710284366&units=metric
+    const response = await axios.get(
+      'https://api.openweathermap.org/data/2.5/weather',
+      {
+        params: {
+          q: state.search,
+          appid: '4d8fb5b93d4af21d66a2948710284366',
+          units: 'metric',
+        },
+      }
+    )
+    commit('SET_WEATHER', response)
+  },
+  //
+}
