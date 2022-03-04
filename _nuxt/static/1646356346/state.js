4350e2eed3da1ffb56ef4431b756a20657d6ac7c
daasc <paulojhol@gmail.com>
2022-03-03 22:25:14 -0300

    Updates

--- _nuxt/static/1646356346/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__=(function(a,b){return {staticAssetsBase:"\u002FWeather\u002F_nuxt\u002Fstatic\u002F1646356346",layout:"default",error:a,state:{weather:{weatherObjs:a,search:b,errorRequest:b}},serverRendered:true,routePath:"\u002F",config:{_app:{basePath:"\u002FWeather\u002F",assetsPath:"\u002FWeather\u002F_nuxt\u002F",cdnURL:a}}}}(null,""));
---
+++ _nuxt/static/1646357106/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a,b){return {staticAssetsBase:"\u002FWeather\u002F_nuxt\u002Fstatic\u002F1646357106",layout:"default",error:a,state:{weather:{weatherObjs:a,search:b,errorRequest:b}},serverRendered:true,routePath:"\u002F",config:{_app:{basePath:"\u002FWeather\u002F",assetsPath:"\u002FWeather\u002F_nuxt\u002F",cdnURL:a}}}}(null,""));
